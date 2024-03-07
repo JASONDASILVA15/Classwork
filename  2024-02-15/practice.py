2e371de28eeb13f8bc1ef4da3ffd1de0e98d8337
Jason <jdasilva15@bostonk12.org>
2024-03-07 09:01:12 -0500

    be yourself.

---  2024-02-15/practice.py
+++  2024-02-15/practice.py
@@ -1,4 +1,13 @@
+<<<<<<< HEAD
 x = input("whats your name?")
 y = input("whats today's date?")
 z = input("whats your?")
 
+=======
+# Jason Dasilva
+# 03072024
+
+a = input("Please enter your name:")
+b = ","
+print("Hi",a+b,"I hope you are having an awesome day!")
+>>>>>>> 33f86d4 (be yourself.)
