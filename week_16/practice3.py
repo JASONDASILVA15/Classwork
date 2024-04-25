98ef54e35b4ae0be06ee508cb47e578c5babb9c5
Jason <jdasilva15@bostonk12.org>
2024-04-25 10:34:40 -0400

    be yourself.

---
+++ week_16/practice3.py
@@ -0,0 +1,16 @@
+# Jason Dasilva
+# 4/25/2024
+
+# This how u create an empty list
+a = []
+
+# This how u create a list with values
+b = [1, 2, 3]
+
+print(a)
+print(b)
+
+a += ["my favorite classmate is minimouse"]
+b += [4]
+print(a)
+print(b)
