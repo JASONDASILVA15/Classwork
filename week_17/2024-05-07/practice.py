a234d07dfbfc1c716f0404bf1fb9cd4b21bc1ab1
Jason <jdasilva15@bostonk12.org>
2024-05-08 09:26:39 -0400

    be yourself.

---
+++ week_17/2024-05-07/practice.py
@@ -0,0 +1,30 @@
+# jason dasilva
+# 5/7/2024
+
+# creating an empty list
+a = []
+
+# using the operator to add to the list
+a += [1]
+a += [2]
+a += [3]
+
+# printing the list
+print(a)
+
+# creating an empty list named b
+b = []
+
+# using the function to add to the list b
+b.append(1)
+b.append(2)
+b.append(3)
+
+# printing the list b
+print(b)
+
+# positinal comparison
+print(a == b)
+
+# object comparison
+print(a is b)
