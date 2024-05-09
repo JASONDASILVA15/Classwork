7fa0fd8dfe5dff59f355d7900627f0c51ffb1187
Jason <jdasilva15@bostonk12.org>
2024-05-09 10:42:42 -0400

    be yourself.

--- week_17/2024/05/08/lists.py
+++ week_17/2024/05/08/lists.py
@@ -5,28 +5,34 @@
 lst = []
 
 # using a for loop to add the numbers 1 threw 100 to the list
-for b in range(1, 100):
-    lst = [b]
+for b in range(1, 101):
+    lst += [b]
 
 # printing the list lst
 print(lst)
 
-# creating an empty list named od
-od = []
+# creating an empty list named odd
+odd = []
 
 # using a for loop to add the odd numbers 1 threw 100 to the list odd
 for b in range(1, 101, 2):
-    od += [b]
+    odd += [b]
 
 # printing a list od
-print(od)
+print(odd)
 
-# creating an empty list named eve
-eve = []
+# creating an empty list named even
+even = []
 
 # using a for loop to add the even numbers 1 threw 100 to the list eve
-for b in range(1, 101):
-    eve += [b]
+for b in range(1, 101, 1):
+    even += [b]
 
 # printing a list eve
-print(eve)
+print(even)
+
+# create a variable named b that point tho the first list u created
+b = lst
+
+# create a variable named joined that joins even and odd list using a operator
+joined +
